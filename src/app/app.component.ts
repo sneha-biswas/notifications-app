d0b34c4124c409a8803a1c16029f14f1f4de62f2
sneha.biswas <sneha.biswas@speridian.com>
2021-12-08 14:49:52 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(update : SwUpdate, push : SwPush, snackbar : MatSnackBar) {
+
+    update.available.subscribe(update => {
+      console.log("available");
+      const snack = snackbar.open("Update available", "Reload");
+
+      snack.onAction().subscribe( x => {
+          window.location.reload();
+      });
+    });
+
+    push.messages.subscribe(msg => {
+      console.log(msg);
+      snackbar.open(JSON.stringify(msg));
+    });
+
+    const key = 'BCX-kxivwc9mQZpnF0cMOHdauP1bpBr4tPdwhbG-Ma9ANoonlgmPbuNZPv6ZbbsTD-Fyx7h6p7QrZXLqV3ng-Rw';
+    push.requestSubscription({serverPublicKey : key}).then(pushsubs => {
+        console.log(pushsubs.toJSON());  
+    })
+  }
   title = 'notifications-app';
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgModule } from '@angular/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
